9d0fb9862a39e3533a18f9a5c7925be4ccb3b6d7
Romeo Soria Jr <rsj@rome.af>
2021-01-02 09:45:12 -0800

    leetcode daily jan 2

---
+++ leetcode/lcc-2021-01-02.js
@@ -0,0 +1,10 @@
+function getTargetCopy (original, cloned, target) {
+
+  if (!original) return null;
+
+  if (original == target) return cloned;
+
+  return getTargetCopy(original.left, cloned.left, target) ||
+  getTargetCopy(original.right, cloned.right, target)
+
+}
