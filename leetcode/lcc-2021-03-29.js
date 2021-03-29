b2ac96d4adad461082b9c707a8277a7ee13bad6e
Romeo Soria Jr <rsj@rome.af>
2021-03-29 14:40:09 -0700

    leetcode daily

--- leetcode/lcc-2021-03-29.js
+++ leetcode/lcc-2021-03-29.js
@@ -1,26 +1,24 @@
 function flipMatchVoyage (root, voyage) {
+  var flipped = [];
+  var index = 0;
 
-  var result = [];
-  var i = 0;
-
-  return dfs(root, voyage) ? result : [-1];
+  return dfs(root, voyage) ? flipped : [-1];
 
   function dfs(root, voyage) {
     if (!root) return true;
-    if (root.val != voyage[i]) {
+
+    if (root.val !== voyage[index]) {
       return false;
     }
 
-    i++;
+    index += 1;
 
-    if (root.left && root.left.val != voyage[i]) {
-      result.push(root.val);
+    if (root.left && root.left.val !== voyage[index]) {
+      flipped.push(root.val);
+      // switch the dfs order from left to right to right to left;
       return dfs(root.right, voyage) && dfs(root.left, voyage);
     }
 
     return dfs(root.left, voyage) && dfs(root.right, voyage);
   }
 }
-
-// dont have to actually flip the nodes..
-// just keep track of which should be flipped.
