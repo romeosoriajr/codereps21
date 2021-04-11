a92c8523e620c34872e00f9ea35fd74ce1305b43
Romeo Soria Jr <rsj@rome.af>
2021-04-10 22:17:13 -0700

    leetcode daily

--- leetcode/lcc-2021-04-10.js
+++ leetcode/lcc-2021-04-10.js
@@ -1,44 +1,43 @@
 function longestIncreasingPath (matrix) {
+
   var m = matrix.length;
   var n = matrix[0].length;
-
+  var directions = [[0, 1], [0, -1], [1, 0], [-1, 0]];
   var longest = 1;
   var memo = new Array(m).fill(null).map(() => new Array(n).fill(0));
 
-  var directions = [[0, 1], [0, -1], [-1, 0], [1, 0]]
 
   for (let i=0; i < m; i++) {
     for (let j=0; j < n; j++) {
-      var length = dfs(i, j);
-      longest = Math.max(length, longest);
+      longest = Math.max(longest, helper(i, j));
     }
   }
 
   return longest;
 
-  // should return a value of the longest length
-  function dfs(row, col) {
+  // return the current length
+  function helper (row, col) {
     if (memo[row][col] !== 0) return memo[row][col];
-    var currLongest = 1;
 
-    for (let i=0; i < directions.length; i++) {
-      var currDirection = directions[i];
-      var nextRow = row + currDirection[0];
-      var nextCol = col + currDirection[1];
+    var localLongest = 1;
+
+    for (let [offsetRow, offsetCol] of directions) {
+
+      var nextRow = row+offsetRow;
+      var nextCol = col+offsetCol;
 
       if (nextRow < 0 || nextCol < 0 || nextRow >= m || nextCol >= n) continue;
-      if (matrix[row][col] <= matrix[nextRow][nextCol]) continue;
 
-      var length = 1 + dfs(nextRow, nextCol);
-      currLongest = Math.max(currLongest, length);
+      if (matrix[row][col] >= matrix[nextRow][nextCol]) continue;
+
+      localLongest = Math.max(localLongest, helper(nextRow, nextCol) + 1);
     }
 
-    memo[row][col] = currLongest;
+    memo[row][col] = localLongest;
 
-    return currLongest;
+    return localLongest;
 
   }
-
 }
 
 console.log(longestIncreasingPath([[3,4,5],[3,2,6],[2,2,1]]))
