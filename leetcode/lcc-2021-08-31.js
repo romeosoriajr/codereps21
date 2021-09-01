11c82e3b20292abdfbee3cc2a840dc0846862d61
Romeo Soria Jr <rsj@rome.af>
2021-08-31 21:24:50 -0700

    leetcode daily

---
+++ leetcode/lcc-2021-08-31.js
@@ -0,0 +1,17 @@
+function findMin(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    const mid = parseInt((left + right) / 2);
+
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return nums[left];
+}
+
