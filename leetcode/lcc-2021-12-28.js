d6b53ea81c49433948f0eea1b5aa6655daaf0b30
Romeo Soria Jr <rsj@rome.af>
2021-12-28 08:38:25 -0800

    leetcode daily

---
+++ leetcode/lcc-2021-12-28.js
@@ -0,0 +1,15 @@
+function middleNode (head) {
+
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+
+}
+
+
